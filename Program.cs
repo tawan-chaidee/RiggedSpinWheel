0bfe95b3d8a998750f8aeb0accd7ab782009b08d
tawan-cha <tawan.jhaidee@gmail.com>
2025-08-06 18:23:31 +0700

    Cleanup comment

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -10,14 +9,11 @@ builder.Services.AddSingleton<ISpinWheelRoomManager, SpinWheelRoomManager>();
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
 
-// Allow all origins for CORS
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyOrigin() // Allow all origins
-              .AllowAnyMethod()  // Allow all HTTP methods
-              .AllowAnyHeader(); // Allow all headers
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
     });
 });
 
@@ -40,6 +36,6 @@ app.UseCors();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.MapControllers();
-app.MapHub<Room>("/room"); // Endpoint for WebSocket connection
+app.MapHub<Room>("/room");
 
 app.Run();
